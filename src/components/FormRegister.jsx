ed8ae55a7715dca1c5cd89ed59cf22dd73e9a46c
Antoine Carpentier <antoine.carpentier@anelis.org>
2024-09-01 22:37:42 +0200

    update form register

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -7,10 +7,9 @@ const FormRegister = () => {
             <div className="form-register">
                 <h1 className='mb-20'>S’enregister</h1>
                 <input type="text" placeholder="Nom:" className="input input-bordered w-full bg-inherit" />
-                <input type="text" placeholder="Nombre de personnes:" className="input input-bordered w-full bg-inherit" />
-                <input type="text" placeholder="Temps:" className="input input-bordered w-full bg-inherit" />
-                <input type="text" placeholder="Description:" className="input input-bordered w-full bg-inherit" />
-                <input type="text" placeholder="Appareil:" className="input input-bordered w-full bg-inherit" />
+                <input type="text" placeholder="Email:" className="input input-bordered w-full bg-inherit" />
+                <input type="text" placeholder="Téléphone:" className="input input-bordered w-full bg-inherit" />
+                <input type="text" placeholder="Mot de passe:" className="input input-bordered w-full bg-inherit" />
                 <button className="btnForm mt-10 btn-register">S’enregistrer</button>
                 <button className="btnForm mt-10 btn-login ">Vous avez déja un compte ? Se connecter</button>
             </div>
