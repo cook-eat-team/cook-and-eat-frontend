cfce36ad8a0530e0ccac23aa04bf245bb568a967
Antoine Carpentier <antoine.carpentier@anelis.org>
2024-09-01 15:14:36 +0200

    update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,15 @@ const Card = () =>{
     return <> <div className="card card-compact bg-base-100 w-96 shadow-xl">
     <figure>
       <img
-        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-        alt="Shoes" />
+        src="..images/Tartiflette.jpg"
+        alt="Tartiflette" />
     </figure>
     <div className="card-body">
-      <h2 className="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div className="card-actions justify-end">
-        <button className="btn btn-primary">Buy Now</button>
+      <h2 className="card-title">Tartiflette</h2>
+      <p>Commencer par cuire les pommes de terre dans l'eau bouillante. Puis épluchez les et coupez les en rondelles. Émincer les oignons puis les faire dorer dans du beurre. Ajouter le jambon fumé coupé en en morceaux ainsi que les pommes de terres. Salez, poivrez à votre gout ( et celui de vos convives ) Laissez cuisiner durant environ 10 minutes puis ajouter le vin blanc. Après 5 minutes, mettre le tout dans un plat à gratin. Coupez le reblochon, soit en tranches, soit le couper en 2 dans le sens de l'épaisseur et recouvrir les pommes de terre. Cuire au four (environ 220°) durant 25 minutes. C'est prêt !</p>
+      <div className="card-actions justify-space-between items-center">
+        <p>60min - 4 personnes</p>
+        <button className="btn btn-primary">VOIR RECETTE</button>
       </div>
     </div>
   </div></>
