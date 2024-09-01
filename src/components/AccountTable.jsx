c538c4e0f301e04bfdc2e0a2f633aa059700a3f4
Antoine Carpentier <antoine.carpentier@anelis.org>
2024-09-01 23:52:18 +0200

    test

--- src/components/AccountTable.jsx
+++ src/components/AccountTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../style/AccountTable.css';
 import { Link } from 'react-router-dom';
+import '../style/AccountTable.css';
 
 const AccountTable = () => {
     return (
