df6fd34c5abe80b9706fc36234c5b4bb5a2c968b
Antoine Carpentier <antoine.carpentier@anelis.org>
2024-09-02 09:55:43 +0200

    update main

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/account" element={<Account />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/addRecipe" element={<AddRecipe />} />
-        <Route path="/showRecipe" element={<ShowRecipe />} />
+        <Route path="/add-recipe" element={<AddRecipe />} />
+        <Route path="/show -recipe" element={<ShowRecipe />} />
         <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ const Card = () =>{
       <p class='text-card'>Commencer par cuire les pommes de terre dans l'eau bouillante. Puis épluchez les et coupez les en rondelles. Émincer les oignons puis les faire dorer dans du beurre. Ajouter le jambon fumé coupé en en morceaux ainsi que les pommes de terres. Salez, poivrez à votre gout ( et celui de vos convives ) Laissez cuisiner durant environ 10 minutes puis ajouter le vin blanc. Après 5 minutes, mettre le tout dans un plat à gratin. Coupez le reblochon, soit en tranches, soit le couper en 2 dans le sens de l'épaisseur et recouvrir les pommes de terre. Cuire au four (environ 220°) durant 25 minutes. C'est prêt !</p>
       <div className="card-actions justify-space-between items-center">
         <p>60min - 4 personnes</p>
-        <button className="btn"><Link to="/showRecipe">VOIR RECETTE</Link></button>
+        <button className="btn"><Link to="/show-recipe">VOIR RECETTE</Link></button>
       </div>
     </div>
   </div></>
